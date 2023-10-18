ba2c9b93b71ae66f0934fe1055a8642758a0af37
alfie-tran <alfie.tran245@gmail.com>
2023-10-18 23:51:43 +0700

    edit js

--- js/21810052.js
+++ js/21810052.js
@@ -1,12 +1,12 @@
 const API = 'https://web1-21810052.vercel.app/api';
-async function loadData(request, templateId, viewID){
-        const response= await fetch(`${API/{request}}`);
-        const data= await response.json();
-        
+async function loadData(request, templateId, viewId){
+        const response = await fetch(`${API}/{request}`);
+        const data = await response.json();
+         
         var source = document.getElementById("templateId").innerHTML;
         var template = Handlebars.compile(source);
 
-        var context = { data: data };
+        var context = { data : data };
         var view = document.getElementById('viewId');
         view.innerHTML = template(context);
         

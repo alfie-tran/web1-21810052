1a20f3e6b9ada6a0fa4b494845d91b72d2a69204
alfie-tran <alfie.tran245@gmail.com>
2023-10-22 23:11:45 +0700

    edit gallery

--- js/21810052.js
+++ js/21810052.js
@@ -1,12 +1,51 @@
-const API = "https://web1-21810052.vercel.app/api";
-async function loadData(request, templateId, viewId){
-        const response = await fetch(`${API}/{request}`);
-        const data = await response.json();
-         
-        var source = document.getElementById("templateId").innerHTML;
-        var template = Handlebars.compile(source);
+const CAPTCHA_PROJECT_ID = "web1-21810052-1697784646259";
+const CAPTCHA_API_KEY =  "AIzaSyC_Jt0i8uIAxIQQGanbs_tFo_a6e6V4pPQ";
+const CAPTCHA_SITE_KEY = "6Lc9WLYoAAAAALfAE2R5fhAvRi6boT_dciQhzlH3";
 
-        var context = { data : data };
-        var view = document.getElementById('viewId');
-        view.innerHTML = template(context);        
-      }
+const API = 'https://web1-api.vercel.app/api';
+const AUTHENTICATE_API = 'https://web1-api.vercel.app/users';
+
+// async function loadData(request, templateId, viewId){
+//         const response = await fetch(`${API}/{request}`);
+//         const data = await response.json();
+         
+//         var source = document.getElementById("templateId").innerHTML;
+//         var template = Handlebars.compile(source);
+
+//         var context = { data : data };
+//         var view = document.getElementById("viewId");
+//         view.innerHTML = template(context);        
+//       }
+
+const loadData = async(request, templateId, viewId) => {
+    const response = await fetch(`${API}/${request}`);
+    const data = await response.json();
+         
+    var source = document.getElementById(templateId).innerHTML;
+    var template = Handlebars.compile(source);
+    // var template = Handlebars.templates[`${templateId}.hbs`];
+    // console.log(template);
+    // console.log(templateId);
+    // console.log(Handlebars.templates);
+    var context = { data : data };
+    var view = document.getElementById(viewId);
+    view.innerHTML = template(context);   
+}
+
+async function getAuthenticateToken(username, password){
+    let response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({username, password})
+    });
+    let result = await response.json();
+    if(response.status == 200){
+        return result.token;
+    }
+    
+    throw new Error(result.message);
+    
+}
